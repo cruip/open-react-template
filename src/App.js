cc6e11ee15aee9e6c4c8a73d992c52e774c80212
pasqualevitiello <pasqualevitiello@gmail.com>
2020-04-06 20:14:18 +0200

    GA fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { withRouter, Switch } from 'react-router-dom';
 import AppRoute from './utils/AppRoute';
 import ScrollReveal from './utils/ScrollReveal';
+import ReactGA from 'react-ga';
 
 // Layouts
 import LayoutDefault from './layouts/LayoutDefault';
@@ -14,13 +15,22 @@ class App extends React.Component {
   componentDidMount() {
     document.body.classList.add('is-loaded')
     this.refs.scrollReveal.init();
+    this.initAnalytics();    
   }
 
   // Route change
   componentDidUpdate(prevProps) {
     if (this.props.location.pathname !== prevProps.location.pathname) {
       this.refs.scrollReveal.init();
+      this.initAnalytics();  
     }
+  }
+
+  initAnalytics = () => {
+    // Initialize google analytics page view tracking
+    ReactGA.initialize(process.env.REACT_APP_GA_CODE);
+    ReactGA.set({ page: this.props.location.pathname }); // Update the user's current page
+    ReactGA.pageview(this.props.location.pathname); // Record a pageview for the given page        
   }
 
   render() {
