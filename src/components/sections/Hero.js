397c2342bce4cfc041af429374618b3b61e3144e
Alexander Mundlin <alex.mundlin@gmail.com>
2021-01-30 15:27:14 -0800

    First commit to change the GitHub location of this template

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -72,7 +72,7 @@ const Hero = ({
                   <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
                     Get started
                     </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
+                  <Button tag="a" color="dark" wideMobile href="https://github.com/amundlin/open-react-template/">
                     View on Github
                     </Button>
                 </ButtonGroup>
