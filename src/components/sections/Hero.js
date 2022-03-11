80d5ab2a60b60a1c909ddd80a4e57eab651a6af8
SeamusHarkin <36312406+aecko@users.noreply.github.com>
2022-03-11 19:52:49 +0000

    Update Hero.js

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import classNames from "classnames";
 import { SectionProps } from "../../utils/SectionProps";
 import ButtonGroup from "../elements/ButtonGroup";
