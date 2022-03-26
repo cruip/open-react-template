f905364b57edb4d3092d0381cb5ab5a6796a18e1
Matthieu <100421924+Mastmat09@users.noreply.github.com>
2022-03-26 17:20:50 +0100

    Update Logo.js

--- src/components/layout/partials/Logo.js
+++ src/components/layout/partials/Logo.js
@@ -21,10 +21,10 @@ const Logo = ({
       <h1 className="m-0">
         <Link to="/">
           <Image
-            src={require('./../../../assets/images/logo.svg')}
+            src={require('./../../../assets/images/logo-df.png')}
             alt="Open"
-            width={32}
-            height={32} />
+            width={75}
+            height={75} />
         </Link>
       </h1>
     </div>
