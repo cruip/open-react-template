1bee228625f63a7171474459ce52f73887551734
Matthieu <100421924+Mastmat09@users.noreply.github.com>
2022-03-26 17:10:34 +0100

    Update Hero.js

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,18 +61,18 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+            <span className="bigger-text">Drone photography for real estate</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                Our photos works well, thanks to new on-board aerial photography technologies, I offer photographs to enhance your home or your private domain. 
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
+                  <Button tag="a" color="primary" wideMobile href="https://www.dronefutur.fr">
                     Get started
                     </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
+                  <Button tag="a" color="dark" wideMobile href="https://github.com/">
                     View on Github
                     </Button>
                 </ButtonGroup>
@@ -81,14 +81,14 @@ const Hero = ({
           </div>
           <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
             <a
-              data-video="https://player.vimeo.com/video/174002812"
+              data-video="https://player.vimeo.com/video/332293586"
               href="#0"
               aria-controls="video-modal"
               onClick={openModal}
             >
               <Image
-                className="has-shadow"
-                src={require('./../../assets/images/video-placeholder.jpg')}
+                className="has-rounded"
+                src={require('./../../assets/images/montbel-drone.jpg')}
                 alt="Hero"
                 width={896}
                 height={504} />
@@ -98,7 +98,7 @@ const Hero = ({
             id="video-modal"
             show={videoModalActive}
             handleClose={closeModal}
-            video="https://player.vimeo.com/video/174002812"
+            video="https://player.vimeo.com/video/332293586"
             videoTag="iframe" />
         </div>
       </div>
