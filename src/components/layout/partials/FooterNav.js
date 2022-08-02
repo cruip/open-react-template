be3a42f597a19bf4973f1b0fcf58af0c56cc39be
Dana Szova <dana.s.szova@gmail.com>
2022-08-02 14:32:14 -0700

    adds github pages

--- src/components/layout/partials/FooterNav.js
+++ src/components/layout/partials/FooterNav.js
@@ -22,12 +22,6 @@ const FooterNav = ({
           <Link to="#0">Contact</Link>
         </li>
         <li>
-          <Link to="#0">About us</Link>
-        </li>
-        <li>
-          <Link to="#0">FAQ's</Link>
-        </li>
-        <li>
           <Link to="#0">Support</Link>
         </li>
       </ul>
