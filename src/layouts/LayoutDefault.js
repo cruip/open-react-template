e50c99aae7eea095a288267388ee5392769cf8ca
pasqualevitiello <pasqualevitiello@gmail.com>
2022-10-18 14:06:07 +0200

    add banner

--- src/layouts/LayoutDefault.js
+++ src/layouts/LayoutDefault.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/layout/Header';
 import Footer from '../components/layout/Footer';
+import Banner from '../components/layout/partials/Banner';
 
 const LayoutDefault = ({ children }) => (
   <>
@@ -8,6 +9,7 @@ const LayoutDefault = ({ children }) => (
     <main className="site-content">
       {children}
     </main>
+    <Banner />
     <Footer />
   </>
 );
