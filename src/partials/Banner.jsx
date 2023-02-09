3f6e4b28a48d4fc03aa1d6821718ca5e51e8ea11
pasqualevitiello <pasqualevitiello@gmail.com>
2023-02-09 17:33:59 +0100

    mc

--- src/partials/Banner.jsx
+++ src/partials/Banner.jsx
@@ -16,7 +16,7 @@ function Banner() {
                 target="_blank"
                 rel="noreferrer"
               >
-                Download<span class="hidden sm:inline"> on GitHub</span>
+                Download<span className="hidden sm:inline"> on GitHub</span>
               </a>{' '}
               <span className="italic px-1.5">or</span>{' '}
               <a className="font-medium hover:underline text-emerald-400" href="https://cruip.com/open-pro/" target="_blank" rel="noreferrer">
