74f86fcc8461b6a17098f41d9d947d50c66b020e
arnabroy <arnabroy.007@gmail.com>
2023-07-12 19:29:57 -0700

    Updtaed company name

--- components/hero.tsx
+++ components/hero.tsx
@@ -24,7 +24,7 @@ export default function Hero() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h1 mb-4" data-aos="fade-up">Gobdu Corporation !!!</h1>
+            <h1 className="h1 mb-4" data-aos="fade-up">GOBDU</h1>
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">We make your journey of becoming a home owner easier, faster and hassle free</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
