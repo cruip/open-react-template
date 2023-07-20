117bd2ec6f6c5f4832a1eebe2d7acae2f8b83fc8
pasqualevitiello <pasqualevitiello@gmail.com>
2023-07-20 08:35:43 +0200

    fix banner z-index

--- components/banner.tsx
+++ components/banner.tsx
@@ -8,7 +8,7 @@ export default function Banner() {
   return (
     <>
       {bannerOpen && (
-        <div className="fixed bottom-0 right-0 w-full md:bottom-8 md:right-12 md:w-auto z-60">
+        <div className="fixed bottom-0 right-0 w-full md:bottom-8 md:right-12 md:w-auto z-50">
           <div className="bg-slate-800 text-slate-50 text-sm p-3 md:rounded shadow-lg flex justify-between">
             <div className="text-slate-500 inline-flex">
               <a
