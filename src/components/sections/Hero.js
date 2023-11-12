ccf45dc5edf1e9a2ec5e645f79bc0c9e97664f11
Matthieu <100421924+Mastmat09@users.noreply.github.com>
2023-11-12 10:40:37 +0100

    Update Hero.js
    add simple text

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,7 +61,7 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-            <span className="bigger-text">Drone photography for real estate</span>
+            <span className="bigger-text">Drone photography for real estate or more</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
