c606e57d719c1db9e6c44474e57fa9992fa6d81e
Siddhant Satapathy <siddhant.satapathy1@gmail.com>
2023-11-17 18:21:33 -0800

    Updates browser tab name

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,5 +1,5 @@
 export const metadata = {
-  title: 'Home - Open PRO',
+  title: 'AnswersAi',
   description: 'Page description',
 }
 
--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -4,6 +4,7 @@ import FeatImage01 from '@/public/images/features-03-image-01.png'
 import FeatImage02 from '@/public/images/features-03-image-02.png'
 import FeatImage03 from '@/public/images/features-03-image-03.png'
 
+
 export default function Zigzag() {
   return (
     <section>
