a533f60a3235f5d13314bca1b7c068ec39e32b53
manitniwas <manitniwas2003@gmail.com>
2023-12-30 15:43:07 -0500

    scaled instructions

--- components/Instructions.client.tsx
+++ components/Instructions.client.tsx
@@ -15,55 +15,99 @@ export default function Instructions() {
       <style jsx>{`
         /* Common styles for both left and right containers */
         .logo-container {
-          text-align: center; /* Center text horizontally */
-          color: #fff; /* White text color */
+          text-align: center;
+          color: #fff;
         }
 
-        /* Styles for the container row */
         .container-row {
-          display: flex; /* Use flex to arrange items horizontally */
+          display: flex;
           justify-content: center;
           align-items: center;
-          margin-bottom: -200px; /* Negative margin to reduce space between rows */
+          margin-bottom: -200px;
         }
+
         .container-row-bottom {
-          display: flex; /* Use flex to arrange items horizontally */
+          display: flex;
           justify-content: center;
           align-items: center;
         }
 
         /* Styles for left and right alignment with padding */
         .left-align {
-          justify-content: flex-start; /* Aligns content to the left */
-          padding-left: 300px; /* Padding on the left side */
+          justify-content: flex-start;
+          padding-left: 300px;
         }
-    
 
         .right-align {
-          justify-content: flex-end; /* Aligns content to the right */
-          padding-right: 300px; /* Padding on the right side */
+          justify-content: flex-end;
+          padding-right: 300px;
         }
 
         .scaled-image {
-          transform: scale(0.25); /* Scale the image to 50% of its original size */
+          transform: scale(0.25);
         }
-        .image-container {
-          filter: drop-shadow(0 0 5px rgba(238, 232, 170, 0.5)); /* Adjust the color and intensity as needed */
-        }
-        .text-container {
-          display: flex; /* Use flexbox for layout */
-          flex-direction: column; /* Stack items vertically */
-          align-items: center; /* Center items horizontally */
-          text-align: center; /* Center text */
-          max-width: 250px; /* Maximum width of 100px */
 
+        .image-container {
+          filter: drop-shadow(0 0 5px rgba(238, 232, 170, 0.5));
+          margin-bottom: 20px; /* Add margin to separate images vertically */
+        }
+
+        .pref-container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          text-align: center;
+          max-width: 250px;
+          margin-top: 20px; /* Add margin to separate text and image vertically */
+        }
+
+        .media-container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          text-align: center;
+          max-width: 250px;
+          margin-bottom: 20px; /* Add margin to separate text and image vertically */
+        }
+
+        .friends-container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          text-align: center;
+          max-width: 250px;
+          margin-top: 200px; /* Add margin to separate text and image vertically */
+        }
+
+        .text-container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          text-align: center;
+          max-width: 250px;
+          margin-top: 20px; /* Add margin to separate text and image vertically */
+        }
+
+        @media (max-width: 768px) {
+          /* Adjust styling for smaller screens */
+          .container-row,
+          .container-row-bottom {
+            flex-direction: column;
+            align-items: center;
+          }
+
+          .left-align,
+          .right-align {
+            padding-left: 0;
+            padding-right: 0;
+          }
         }
       `}</style>
 
       {/* First row - Left aligned */}
       <h2 className="h2 mb-4 text-center"  data-aos="fade-down" data-aos-delay="100">GET STARTED WITH 3 EASY STEPS</h2>
       <div className="container-row left-align"  data-aos="fade-right" data-aos-delay="100">
-        <div className="text-container mr-4"> {/* New container for vertical alignment */}
+        <div className="pref-container mr-4"> {/* New container for vertical alignment */}
           <h2 className="h4 text-right">1. Set Your Preferences</h2>
           <p className="text-right text-med text-gray-400">Tell us what you are interested in by selecting from an everchanging list of custom topics</p>
         </div>
@@ -93,7 +137,7 @@ export default function Instructions() {
             />
           </Link>
         </div>
-        <div className="text-container ml-4"> {/* New container for vertical alignment */}
+        <div className="media-container ml-4"> {/* New container for vertical alignment */}
           <h2 className="h4 text-left">2. Check out your media</h2>
           <p className="text-left text-med text-gray-400">Listen to your audio byte for the day and stay connected with the topics you love most!</p>
         </div>
@@ -101,7 +145,7 @@ export default function Instructions() {
 
       {/* Third row - Left aligned */}
       <div className="container-row-bottom left-align"  data-aos="fade-right" data-aos-delay="100">
-      <div className="text-container ml-4"> {/* New container for vertical alignment */}
+      <div className="friends-container ml-4"> {/* New container for vertical alignment */}
           <h2 className="h4 text-right">3. See What Your Friends Are Up To!</h2>
           <p className="text-right text-med text-gray-400">Check out what your friends are listening to and interesting clips from their audio bytes</p>
         </div>
--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -33,11 +33,11 @@ export default function Footer() {
                 </Link>
               </li>
               <li className="ml-4">
-                <Link href="https://www.linkedin.com/" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out" aria-label="Linkedin">
-                  <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M23.3 8H8.7c-.4 0-.7.3-.7.7v14.7c0 .3.3.6.7.6h14.7c.4 0 .7-.3.7-.7V8.7c-.1-.4-.4-.7-.8-.7zM12.7 21.6h-2.3V14h2.4v7.6h-.1zM11.6 13c-.8 0-1.4-.7-1.4-1.4 0-.8.6-1.4 1.4-1.4.8 0 1.4.6 1.4 1.4-.1.7-.7 1.4-1.4 1.4zm10 8.6h-2.4v-3.7c0-.9 0-2-1.2-2s-1.4 1-1.4 2v3.8h-2.4V14h2.3v1c.3-.6 1.1-1.2 2.2-1.2 2.4 0 2.8 1.6 2.8 3.6v4.2h.1z" />
-                  </svg>
-                </Link>
+              <Link href="https://www.linkedin.com/company/tunedinapp/" target="_blank" rel="noopener noreferrer" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out" aria-label="Linkedin">
+                <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M23.3 8H8.7c-.4 0-.7.3-.7.7v14.7c0 .3.3.6.7.6h14.7c.4 0 .7-.3.7-.7V8.7c-.1-.4-.4-.7-.8-.7zM12.7 21.6h-2.3V14h2.4v7.6h-.1zM11.6 13c-.8 0-1.4-.7-1.4-1.4 0-.8.6-1.4 1.4-1.4.8 0 1.4.6 1.4 1.4-.1.7-.7 1.4-1.4 1.4zm10 8.6h-2.4v-3.7c0-.9 0-2-1.2-2s-1.4 1-1.4 2v3.8h-2.4V14h2.3v1c.3-.6 1.1-1.2 2.2-1.2 2.4 0 2.8 1.6 2.8 3.6v4.2h.1z" />
+                </svg>
+              </Link>
               </li>
             </ul>
 
