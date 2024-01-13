1a95534c63b4d84ef8564ec3495b2e623ead2156
thomson <82395633+thomson159@users.noreply.github.com>
2024-01-13 20:40:03 +0100

    own repo

--- components/banner.tsx
+++ components/banner.tsx
@@ -9,14 +9,14 @@ export default function Banner() {
             <div className="text-slate-500 inline-flex">
               <a
                 className="font-medium hover:underline text-slate-50"
-                href="https://github.com/cruip/open-react-template"
+                href="https://github.com/thomson159/open-react-template"
                 target="_blank"
                 rel="noreferrer"
               >
                 Download<span className="hidden sm:inline"> on GitHub</span>
               </a>{' '}
               <span className="italic px-1.5">or</span>{' '}
-              <a className="font-medium hover:underline text-emerald-400" href="https://cruip.com/open-pro/" target="_blank" rel="noreferrer">
+              <a className="font-medium hover:underline text-emerald-400" href="https://thomson159.com/open-pro/" target="_blank" rel="noreferrer">
                 Check Premium Version
               </a>
             </div>
