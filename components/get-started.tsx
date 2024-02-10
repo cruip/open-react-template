2e37022a7a8ce48165f03d3a9a802188baca9b8c
Conor Plunkett <conor.p43@gmail.com>
2024-02-09 23:25:04 -0500

    get started changes

--- components/get-started.tsx
+++ components/get-started.tsx
@@ -7,7 +7,8 @@ export default function GetStarted() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h2 className="h2 mb-4">Get Started</h2>
-            <p className="text-xl text-gray-400 mb-8">Ready to make your mark on the gaming world? Join Cycle today and be part of the community that's setting new standards for game development. Your feedback has the power to inspire the next big thing in gaming. Let's build it together.</p>
+            <p className="text-xl text-gray-400 mb-8">If you’re a player, try the demo and start giving feedback today. Reach out to us if you can think of new or better ways to improve Cycle.</p>
+            <p className="text-xl text-gray-400 mb-8">For companies, if you think Cycle’s a fit for you, contact our sales team. <br/>Cycle also serves product-centric companies in other industries looking to improve their product cycles (enterprise software, fashion, bespoke products, etc). We’re excited to see how you can use our tools to help you be better for customers.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
                 <a className="btn text-white bg-red-100 hover:bg-red-100/75 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Try Demo</a>
