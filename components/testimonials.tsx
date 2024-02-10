23cd49d7f9c931d9a3850276592740f507ec9b67
Conor Plunkett <conor.p43@gmail.com>
2024-02-09 23:36:53 -0500

    change accent colors

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -24,14 +24,14 @@ export default function Testimonials() {
               <div>
                 <div className="relative inline-flex flex-col mb-4">
                   <Image className="rounded-full" src={TestimonialImage01} width={48} height={48} alt="Testimonial 01" />
-                  <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                  <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-red-100" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
                   </svg>
                 </div>
               </div>
               <blockquote className="text-lg text-gray-400 grow">— I've been gaming for years and have always felt like my feedback disappeared into a black hole. Since using Cycle, I've noticed a shift. Dev’s are more on point with their balance changes, and they do more of what we want. It's nice to see my input lead to game enhancements that matter to us as players.</blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <cite className="text-gray-200 not-italic">Alex M.</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">League Veteran and Streamer</a>
+                <cite className="text-gray-200 not-italic">Alex M.</cite> - <a className="text-red-100 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">League Veteran and Streamer</a>
               </div>
             </div>
 
@@ -40,14 +40,14 @@ export default function Testimonials() {
               <div>
                 <div className="relative inline-flex flex-col mb-4">
                   <Image className="rounded-full" src={TestimonialImage02} width={48} height={48} alt="Testimonial 02" />
-                  <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                  <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-red-100" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
                   </svg>
                 </div>
               </div>
               <blockquote className="text-lg text-gray-400 grow">— Feedback is crucial in the competitive scene, and Cycle’s tool I think is better than the status quo of only listening to the top few teams. My team and I have provided input on gameplay balance, and we've seen our suggestions come to life. Cycle’s a game-changer for developers who want to have a good pulse on their community.</blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <cite className="text-gray-200 not-italic">Jordan K.</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">Competitive eSports Player</a>
+                <cite className="text-gray-200 not-italic">Jordan K.</cite> - <a className="text-red-100 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">Competitive eSports Player</a>
               </div>
             </div>
 
@@ -56,14 +56,14 @@ export default function Testimonials() {
               <div>
                 <div className="relative inline-flex flex-col mb-4">
                   <Image className="rounded-full" src={TestimonialImage03} width={48} height={48} alt="Testimonial 03" />
-                  <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                  <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-red-100" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
                   </svg>
                 </div>
               </div>
               <blockquote className="text-lg text-gray-400 grow">— It's like the developers are right there with us, tweaking and improving. It's refreshing to feel like part of the development process. It beats any other form of giving feedback I’ve experienced.</blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <cite className="text-gray-200 not-italic">Sam W.</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">Indie Games</a>
+                <cite className="text-gray-200 not-italic">Sam W.</cite> - <a className="text-red-100 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">Indie Games</a>
               </div>
             </div>
 
