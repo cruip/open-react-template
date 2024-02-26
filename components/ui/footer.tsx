24d846ec424d66be49f1ca9cd083e93244ae9a40
Musab Jafri <musabmjafri@gmail.com>
2024-02-26 16:39:59 -0600

    rearranged social links and added targets to open in new tab

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -169,42 +169,13 @@ export default function Footer() {
                   </svg>
                 </a>
               </li>
-              <li className="ml-4">
-                <a
-                  href="https://twitter.com/IMAAN_masjid"
-                  className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
-                  aria-label="Twitter"
-                >
-                  <svg
-                    className="w-8 h-8 fill-current"
-                    viewBox="0 0 32 32"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="m13.063 9 3.495 4.475L20.601 9h2.454l-5.359 5.931L24 23h-4.938l-3.866-4.893L10.771 23H8.316l5.735-6.342L8 9h5.063Zm-.74 1.347h-1.457l8.875 11.232h1.36l-8.778-11.232Z" />
-                  </svg>
-                </a>
-              </li>
 
-              <li className="ml-4">
-                <a
-                  href="https://www.facebook.com/imaanmasjidmelissa"
-                  className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
-                  aria-label="Facebook"
-                >
-                  <svg
-                    className="w-8 h-8 fill-current"
-                    viewBox="0 0 32 32"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
-                  </svg>
-                </a>
-              </li>
               <li className="ml-4">
                 <a
                   href="https://www.instagram.com/imaan_masjid"
                   className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
                   aria-label="Instagram"
+                  target="_blank"
                 >
                   <svg
                     className="w-8 h-8 fill-current"
@@ -218,6 +189,38 @@ export default function Footer() {
                 </a>
               </li>
             </ul>
+              <li className="ml-4">
+                <a
+                  href="https://www.facebook.com/imaanmasjidmelissa"
+                  className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
+                  aria-label="Facebook"
+                  target="_blank"
+                >
+                  <svg
+                    className="w-8 h-8 fill-current"
+                    viewBox="0 0 32 32"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
+                  </svg>
+                </a>
+              </li>
+              <li className="ml-4">
+                <a
+                  href="https://twitter.com/IMAAN_masjid"
+                  className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
+                  aria-label="Twitter"
+                  target="_blank"
+                >
+                  <svg
+                    className="w-8 h-8 fill-current"
+                    viewBox="0 0 32 32"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="m13.063 9 3.495 4.475L20.601 9h2.454l-5.359 5.931L24 23h-4.938l-3.866-4.893L10.771 23H8.316l5.735-6.342L8 9h5.063Zm-.74 1.347h-1.457l8.875 11.232h1.36l-8.778-11.232Z" />
+                  </svg>
+                </a>
+              </li>
 
             {/* Copyrights note */}
             <div className="text-gray-400 text-sm mr-4">
