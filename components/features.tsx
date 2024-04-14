4c5bb1a41e6ca639732c28ee94f56892b5426416
Colin Graydon <colingraydon@gmail.com>
2024-04-13 20:33:30 -0400

    fixed mobile device margin

--- components/features.tsx
+++ components/features.tsx
@@ -2,7 +2,7 @@ export default function Features() {
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6 bg">
-        <div className="py-8 md:pb-20">
+        <div className="py-8 md:py-20">
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h2 className="h2 mb-4">
