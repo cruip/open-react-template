18392ce8d2771f1d0d0a027bd719d2a797ae0f49
alfon-fy <alfonso.tercero@fynce.io>
2024-05-20 15:57:25 +0200

    remove Banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
         <div className="flex flex-col min-h-screen overflow-hidden">
           <Header />
           {children}
-          <Banner />
+          {/* <Banner /> */}
         </div>
       </body>
     </html>
