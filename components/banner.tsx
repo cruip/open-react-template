7d1b92541d0f81e045b9d5827c96290819526a88
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2024-07-18 20:59:14 +0000

    initial chnages

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,9 +1,9 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function Banner() {
-  const [bannerOpen, setBannerOpen] = useState<boolean>(true)
+  const [bannerOpen, setBannerOpen] = useState<boolean>(false);
 
   return (
     <>
@@ -18,15 +18,26 @@ export default function Banner() {
                 rel="noreferrer"
               >
                 Download<span className="hidden sm:inline"> on GitHub</span>
-              </a>{' '}
-              <span className="italic px-1.5">or</span>{' '}
-              <a className="font-medium hover:underline text-emerald-400" href="https://cruip.com/open-pro/" target="_blank" rel="noreferrer">
+              </a>{" "}
+              <span className="italic px-1.5">or</span>{" "}
+              <a
+                className="font-medium hover:underline text-emerald-400"
+                href="https://cruip.com/open-pro/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 Check Premium Version
               </a>
             </div>
-            <button className="text-slate-500 hover:text-slate-400 pl-2 ml-3 border-l border-gray-700" onClick={() => setBannerOpen(false)}>
+            <button
+              className="text-slate-500 hover:text-slate-400 pl-2 ml-3 border-l border-gray-700"
+              onClick={() => setBannerOpen(false)}
+            >
               <span className="sr-only">Close</span>
-              <svg className="w-4 h-4 shrink-0 fill-current" viewBox="0 0 16 16">
+              <svg
+                className="w-4 h-4 shrink-0 fill-current"
+                viewBox="0 0 16 16"
+              >
                 <path d="M12.72 3.293a1 1 0 00-1.415 0L8.012 6.586 4.72 3.293a1 1 0 00-1.414 1.414L6.598 8l-3.293 3.293a1 1 0 101.414 1.414l3.293-3.293 3.293 3.293a1 1 0 001.414-1.414L9.426 8l3.293-3.293a1 1 0 000-1.414z" />
               </svg>
             </button>
@@ -34,5 +45,5 @@ export default function Banner() {
         </div>
       )}
     </>
-  )
+  );
 }
