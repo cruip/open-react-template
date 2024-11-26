0c8608220180209c4026081ff6c754e03b52921b
Faizan Haq <fizoneechan@gmail.com>
2024-11-27 03:40:27 +0500

    section 2

--- components/features.tsx
+++ components/features.tsx
@@ -6,29 +6,14 @@ import FeaturesImage from "@/public/images/features.png";
 export default function Features() {
   return (
     <section className="relative">
-      <div
-        className="pointer-events-none absolute left-1/2 top-0 -z-10 -mt-20 -translate-x-1/2"
-        aria-hidden="true"
-      >
-        <Image
-          className="max-w-none"
-          src={BlurredShapeGray}
-          width={760}
-          height={668}
-          alt="Blurred shape"
-        />
+      <div className="pointer-events-none absolute left-1/2 top-0 -z-10 -mt-20 -translate-x-1/2" aria-hidden="true">
+        <Image className="max-w-none" src={BlurredShapeGray} width={760} height={668} alt="Blurred shape" />
       </div>
       <div
         className="pointer-events-none absolute bottom-0 left-1/2 -z-10 -mb-80 -translate-x-[120%] opacity-50"
         aria-hidden="true"
       >
-        <Image
-          className="max-w-none"
-          src={BlurredShape}
-          width={760}
-          height={668}
-          alt="Blurred shape"
-        />
+        <Image className="max-w-none" src={BlurredShape} width={760} height={668} alt="Blurred shape" />
       </div>
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
         <div className="border-t py-12 [border-image:linear-gradient(to_right,transparent,theme(colors.slate.400/.25),transparent)1] md:py-20">
@@ -36,10 +21,12 @@ export default function Features() {
           <div className="mx-auto max-w-3xl pb-4 text-center md:pb-12">
             <div className="inline-flex items-center gap-3 pb-3 before:h-px before:w-8 before:bg-gradient-to-r before:from-transparent before:to-forest-200/50 after:h-px after:w-8 after:bg-gradient-to-l after:from-transparent after:to-forest-200/50">
               <span className="inline-flex bg-gradient-to-r from-forest-500 to-forest-200 bg-clip-text text-transparent">
-                Services              </span>
+                Services{" "}
+              </span>
             </div>
-            <h2 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.forest.200),theme(colors.gray.50),theme(colors.forest.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-4 font-raleway text-3xl font-semibold text-transparent md:text-4xl">
-              Comprehensive IT Solutions to Scale Your Business            </h2>
+            <h2 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.forest.200),theme(colors.gray.50),theme(colors.forest.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-4 font-menda text-3xl font-medium text-transparent md:text-4xl">
+              Comprehensive IT Solutions to Scale Your Business{" "}
+            </h2>
             {/* <p className="text-lg text-forest-200/65">
               Open AI reads and understands your files, and with nothing more
               than a single line of feedback, so you can go further than the
@@ -58,72 +45,48 @@ export default function Features() {
           {/* Items */}
           <div className="mx-auto grid max-w-sm gap-12 sm:max-w-none sm:grid-cols-2 md:gap-x-14 md:gap-y-16 lg:grid-cols-3">
             <article>
-              <svg
-                className="mb-3 fill-forest-500"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-              >
+              <svg className="mb-3 fill-forest-500" xmlns="http://www.w3.org/2000/svg" width={24} height={24}>
                 <path d="M0 0h14v17H0V0Zm2 2v13h10V2H2Z" />
                 <path
                   fillOpacity=".48"
                   d="m16.295 5.393 7.528 2.034-4.436 16.412L5.87 20.185l.522-1.93 11.585 3.132 3.392-12.55-5.597-1.514.522-1.93Z"
                 />
               </svg>
-              <h3 className="mb-1 font-menda font-bold text-[1.5rem] text-gray-200">
-                Project Milestones
-              </h3>
+              <h3 className="mb-1 font-menda font-semibold text-[1rem] text-gray-200">Web Development and Design</h3>
               <p className="text-forest-200/65">
-                Track progress across custom flows for your team. Find the right
-                balance for the user, privacy and security.
+                Delivering visually appealing, user-friendly and extensive web applications is at the core of what we do. From
+                custom web applications to landing pages, our services include UX/UI design, fullstack development, SEO
+                optimization and cross-browser compatibility{" "}
               </p>
             </article>
             <article>
-              <svg
-                className="mb-3 fill-forest-500"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-              >
+              <svg className="mb-3 fill-forest-500" xmlns="http://www.w3.org/2000/svg" width={24} height={24}>
                 <path fillOpacity=".48" d="M7 8V0H5v8h2Zm12 16v-4h-2v4h2Z" />
                 <path d="M19 6H0v2h17v8H7v-6H5v8h19v-2h-5V6Z" />
               </svg>
-              <h3 className="mb-1 font-raleway text-[1rem] font-semibold text-gray-200">
-                Team Views
-              </h3>
+              <h3 className="mb-1 font-menda text-[1rem] font-semibold text-gray-200">Mobile App Development</h3>
               <p className="text-forest-200/65">
-                Track progress across custom flows for your team. Find the right
-                balance for the user, privacy and security.
+                We create scalable, intuitive and high-performance mobile applications for iOS, Android or cross-platform use.
+                Whether it’s a customer-facing app or a business tool, we design, develop and deploy apps, that deliver seamless
+                user experiences{" "}
               </p>
             </article>
             <article>
-              <svg
-                className="mb-3 fill-forest-500"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-              >
+              <svg className="mb-3 fill-forest-500" xmlns="http://www.w3.org/2000/svg" width={24} height={24}>
                 <path d="M23.414 6 18 .586 16.586 2l3 3H7a6 6 0 0 0-6 6h2a4 4 0 0 1 4-4h12.586l-3 3L18 11.414 23.414 6Z" />
                 <path
                   fillOpacity=".48"
                   d="M13.01 12.508a2.5 2.5 0 0 0-3.502.482L1.797 23.16.203 21.952l7.71-10.17a4.5 4.5 0 1 1 7.172 5.437l-4.84 6.386-1.594-1.209 4.841-6.385a2.5 2.5 0 0 0-.482-3.503Z"
                 />
               </svg>
-              <h3 className="mb-1 font-raleway text-[1rem] font-semibold text-gray-200">
-                Advanced Search
-              </h3>
+              <h3 className="mb-1 font-menda text-[1rem] font-semibold text-gray-200">Systems Integration</h3>
               <p className="text-forest-200/65">
-                Track progress across custom flows for your team. Find the right
-                balance for the user, privacy and security.
+                Streamline your operations with custom API development and systems integration. We connect your existing tools,
+                applications and platforms, enabling seamless data flow, automation and improved operational efficiency.{" "}
               </p>
             </article>
             <article>
-              <svg
-                className="mb-3 fill-forest-500"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-              >
+              <svg className="mb-3 fill-forest-500" xmlns="http://www.w3.org/2000/svg" width={24} height={24}>
                 <path
                   fillOpacity=".48"
                   d="m3.031 9.05-.593-.805 1.609-1.187.594.804a6.966 6.966 0 0 1 0 8.276l-.594.805-1.61-1.188.594-.805a4.966 4.966 0 0 0 0-5.9Z"
@@ -135,54 +98,35 @@ export default function Features() {
                 />
                 <path d="m16.321 2-.5-.866 1.733-1 .5.866A22 22 0 0 1 21 12c0 3.852-1.017 7.636-2.948 10.97l-.502.865-1.73-1.003.501-.865A19.878 19.878 0 0 0 19 12a20 20 0 0 0-2.679-10Z" />
               </svg>
-              <h3 className="mb-1 font-raleway text-[1rem] font-semibold text-gray-200">
-                Strategic Initiatives
-              </h3>
+              <h3 className="mb-1 font-menda text-[1rem] font-semibold text-gray-200">IT Consulting & Cloud Solutions</h3>
               <p className="text-forest-200/65">
-                Track progress across custom flows for your team. Find the right
-                balance for the user, privacy and security.
+                Navigate the complexities of modern IT infrastructure with our consulting and cloud services. We help businesses
+                with cloud migration, infrastructure optimization and digital transformation strategies, while ensuring
+                scalability, security and performance.{" "}
               </p>
             </article>
             <article>
-              <svg
-                className="mb-3 fill-forest-500"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-              >
-                <path
-                  fillOpacity=".48"
-                  d="M12 8.8a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z"
-                />
+              <svg className="mb-3 fill-forest-500" xmlns="http://www.w3.org/2000/svg" width={24} height={24}>
+                <path fillOpacity=".48" d="M12 8.8a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" />
                 <path d="m7.454 2.891.891-.454L7.437.655l-.891.454a12 12 0 0 0 0 21.382l.89.454.91-1.781-.892-.455a10 10 0 0 1 0-17.818ZM17.456 1.11l-.891-.454-.909 1.782.891.454a10 10 0 0 1 0 17.819l-.89.454.908 1.781.89-.454a12 12 0 0 0 0-21.382Z" />
               </svg>
-              <h3 className="mb-1 font-raleway text-[1rem] font-semibold text-gray-200">
-                Flexible Workflows
-              </h3>
+              <h3 className="mb-1 font-menda text-[1rem] font-semibold text-gray-200">Digital Marketing Services</h3>
               <p className="text-forest-200/65">
-                Track progress across custom flows for your team. Find the right
-                balance for the user, privacy and security.
+                Boost your online presence with our comprehensive digital marketing services. From SEO and content marketing to
+                social media management and paid ad campaigns, we craft data-driven strategies that maximize your ROI and drive
+                measurable growth{" "}
               </p>
             </article>
             <article>
-              <svg
-                className="mb-3 fill-forest-500"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-              >
-                <path
-                  fillOpacity=".48"
-                  d="M19 8h5v2h-5V8Zm-4 5h9v2h-9v-2Zm9 5H11v2h13v-2Z"
-                />
+              <svg className="mb-3 fill-forest-500" xmlns="http://www.w3.org/2000/svg" width={24} height={24}>
+                <path fillOpacity=".48" d="M19 8h5v2h-5V8Zm-4 5h9v2h-9v-2Zm9 5H11v2h13v-2Z" />
                 <path d="M19.406 3.844 6.083 20.497.586 15 2 13.586l3.917 3.917L17.844 2.595l1.562 1.25Z" />
               </svg>
-              <h3 className="mb-1 font-raleway text-[1rem] font-semibold text-gray-200">
-                Unified Timeline
-              </h3>
+              <h3 className="mb-1 font-menda text-[1rem] font-semibold text-gray-200">Custom CMS Development</h3>
               <p className="text-forest-200/65">
-                Track progress across custom flows for your team. Find the right
-                balance for the user, privacy and security.
+                We specialize in developing custom content management systems (CMS) tailored to your business needs. From
+                WordPress and Shopify to Drupal and Joomla, we offer seamless integrations, tailored e-commerce solutions, plugin
+                development, theme customization and much more… to drive your online presence.{" "}
               </p>
             </article>
           </div>
