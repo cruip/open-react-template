8918683a2248e48b0310d82a5da03e01159ba1e3
Faizan Haq <fizoneechan@gmail.com>
2024-11-27 14:13:39 +0500

    metadata

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,11 @@
+
 export const metadata = {
-  title: "Home - Open PRO",
-  description: "Page description",
+  title: "Lumunate - Home",
+  description: "We specialize in web and mobile development, systems integration and digital marketing to help businesses thrive in today’s fast-paced digital world.",
+  keywords: ["Lumunate", "Portfolio", "Next.js", "React", "Web Development"],
+  author: "Fizo",
+  viewport: "width=device-width, initial-scale=1.0",
+  charset: "UTF-8",
 };
 
 import PageIllustration from "@/components/page-illustration";
