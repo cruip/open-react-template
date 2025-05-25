57481c97b100cca27c95cc1ac1178233e5cf8bc8
Rahul <chrahulofficial@gmail.com>
2025-05-26 04:10:58 +0530

    renamed the variable

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -6,12 +6,12 @@ import { usePathname } from 'next/navigation'
 
 export default function Header() {
 
-  const isMenuToogle = ["/signin","/signup"]
+  const hideHeaderRoutes = ["/signin","/signup"]
 
   const Path = usePathname();
 
   return (
-    <header className={`  ${isMenuToogle.includes(Path) ? "hidden" : "" } z-30 mt-2 w-full md:mt-5`}>
+    <header className={`  ${hideHeaderRoutes.includes(Path) ? "hidden" : "" } z-30 mt-2 w-full md:mt-5`}>
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
         <div className="relative flex h-14 items-center justify-between gap-3 rounded-2xl bg-gray-900/90 px-3 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,var(--color-gray-800),var(--color-gray-700),var(--color-gray-800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] after:absolute after:inset-0 after:-z-10 after:backdrop-blur-xs">
           {/* Site branding */}
